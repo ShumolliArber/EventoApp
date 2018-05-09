fce751da0d45807491794341893450b952476fb1
Arber Shumolli <arbi_sh@hotmail.com>
2018-05-09 11:07:05 +0200

    Initial commit

---
+++ app/src/main/java/com/example/arbi/evento/Login_Frame.java
@@ -0,0 +1,13 @@
+package com.example.arbi.evento;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login_Frame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login__frame);
+    }
+}
